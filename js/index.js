f571a70266e29f12071c0aad2f553a4a6f1c6b71
ViBurnier <burnvitor2@gmail.com>
2024-12-15 04:44:30 -0300

    fix alert card view

--- js/index.js
+++ js/index.js
@@ -4,39 +4,17 @@ startGame.addEventListener("click", () => {
     mensageUserSelectLvl()
 })
 
-
-//the game level
-function gameLvl(difficulty){
-    
-
-    if(difficulty == 'Easy'){
-        return 1500
-        
-    }
-    else if(difficulty == 'Normal'){
-        return 1000
-        
-    }
-    else if(difficulty == 'Hard'){
-        return 650
-        
-    }
-}
-
 function mensageUserSelectLvl(){
     
     let levelGame = document.getElementById('levelGame')
 
-    let card = document.getElementById('cardAlert')
-
+    
     if(levelGame.value == 0){
         //fazer uma tela que fale pro usuario escolher uma dificuldade
-        return card.classList.remove("d-none")
+        let card = document.getElementById('cardAlert').classList.remove("d-none")
+          
     }
 
-    else{   
-        return [levelGame.value, window.location.href = "app.html"]
-    }
 }
 
 
